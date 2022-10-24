fed25f492db6ea0599397355839f72b3826bbe26
farex2 <farabd90@gmail.com>
2022-10-24 17:54:49 +0100

    add popup form

---
+++ script.js
@@ -0,0 +1,30 @@
+let myLibrary = [];
+
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+function addBookToLibrary(){
+  let name = document.getElementById('name');
+  let author = document.getElementById('author');
+  let pages = document.getElementById('pages');
+
+  name =  name.innerText;
+  author = author.innerText;
+  pages = pages.innerText;
+  
+  let newBook = new Book("name", "author", "pages");
+  myLibrary.push(newBook);
+
+}
+
+function Remove(){
+  let index = e.target.getElementByClassName("table");
+  index = index.dataset.index; 
+  myLibrary.splice(index, 1);
+}
